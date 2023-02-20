c23289a4826be7eb69736642ce70cb843e6c70a3
Pasidu Manoj <111718268+slPASIYA@users.noreply.github.com>
2023-02-20 11:02:33 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+//xxx Wajib Diisi
+
+$token = "MGVhNDk1OTEtNDRmNC00ZDIwLTg3NTAtNjM5Y2MwZDE2OWVl";
+$uuid = "9a39ce81b465f589";
+$userid = "39763151";
+
+$eksternal_version = "2.0.1";
+$version = "42";
+$device = "9";
+$timezone = "7";
+$lang = "en";
+
+$spin_version = "40";
+$user_agent = "Mozilla/5.0 (Linux; Android 11; SM-A105F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36"; //Search in Google Chrome With Keyword "my user agent"
